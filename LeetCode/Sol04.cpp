3b1494f8370777546cbd1ff88bba611b6e7e1938
Yuan Li <vanish8.7@gmail.com>
2016-03-03 13:29:44 +0800

    update sol3-5

---
+++ LeetCode/Sol04.cpp
@@ -0,0 +1,13 @@
+/*
+4. Median of Two Sorted Arrays My Submissions Question
+Total Accepted : 84831 Total Submissions : 465860 Difficulty : Hard
+There are two sorted arrays nums1 and nums2 of size m and n respectively.Find the median of the two sorted arrays.The overall run time complexity should be O(log(m + n)).
+*/
+
+
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+	}
+};
---
+++ LeetCode/Sol05.cpp
@@ -0,0 +1,11 @@
+/*
+5. Longest Palindromic Substring My Submissions Question
+Total Accepted : 96376 Total Submissions : 426919 Difficulty : Medium
+Given a string S, find the longest palindromic substring in S.You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+class Solution {
+public:
+	string longestPalindrome(string s) {
+
+	}
+};
